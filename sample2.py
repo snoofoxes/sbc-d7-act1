a9d5fc542f33068812c84086b2fbb0c5227fd4ad
Christian Dan Petallar <93201432+snoofoxes@users.noreply.github.com>
2024-06-26 16:24:06 +0800

    still have errors

--- sample2.py
+++ sample2.py
@@ -1,17 +1,17 @@
 from random import randint
 #BANK
-
-account = {"name":[],"balance":[]}
-unique = {"user":[]}
+name = []
+account = []
+unique = []
 
 
 def register():
 	give_name = input("Enter a name: ")
 	give_balance = int(input("Enter a balance: "))
 	give_user_id = randint(10000,99999)
-	account["name"].append(give_name) 
-	account["balance"].append(give_balance) 
-	unique["user"].append(give_user_id)
+	name.append(give_name) 
+	account.append(give_balance) 
+	unique.append(give_user_id)
 	print(f"\nRegister Succesfully \nYour Name: {give_name} \nYour Current Balance: {give_balance}\nYour PIN ID: {give_user_id }\n")
 	main()
 
@@ -21,18 +21,12 @@ def id(get_index):
 	if user_id not in unique:
 		print("Incorrect Pin")
 	else:
-		index = account.index(user_id)
+		index = unique.index(user_id)
 		get_index(index)
 
-def check_balance():
+def check_balance(get_index):
 	while True:
-		user_id = int(input("Enter your PIN ID: "))
-		print(unique["user"])
-		if user_id in unique["user"]:
-			print(f'Name: {account["name"]}\nBalance: {account["balance"]}\nPin ID: {unique["user"]}')
-		else:
-			print("Incorrect PIN")
-			print(user_id)
+		print(f'Name: {name[get_index]}\nBalance: {account[get_index]}\nPin ID: {unique[get_index]}')
 		main()
 
 def deposit():
@@ -77,6 +71,7 @@ def delete():
 
 def main():
 	while True:
+		print(unique)
 		print("\nWELCOME TO PYTHON BANK!\n[DEPOSIT | WITHDRAW | CREATE ACCOUNT | CHECK BALANCE]\n")
 		print("Type 'Create' to register\nType 'Deposit' to deposit\nType 'Withdraw' to withdraw\nType 'Check' to check balance\nType 'Delete' to delete account\n")
 		command = input("Enter your command: ").capitalize()
@@ -92,7 +87,7 @@ def main():
 		elif command == "Delete":
 			delete()
 		elif command == "Check":
-			check_balance()
+			id(check_balance)
 			command = input("Enter your command: ").capitalize()
 		else:
 			print("Error Command")
